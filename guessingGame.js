a087c893f06f8b72d1d9d419efe451405910de98
Aryan Soni <arpitsoni811@gmail.com>
2023-10-02 16:26:32 +0530

    completed

--- guessingGame.js
+++ guessingGame.js
@@ -1,3 +1,4 @@
+const { exit } = require("process");
 const readline = require("readline");
 
 let rl;
@@ -11,6 +12,8 @@ function getInput(prompt, callback) {
 function generateRandomNumber() {
   // TODO: Write a function to generate a random number between 1 and 100.
   // Your code here.
+  let x = Math.floor(Math.random() * 100 + 1);
+  return x;
 }
 
 // This function contains the game's logic.
@@ -22,17 +25,34 @@ function playGame(chosenNumber = null) {
 
   // This checks if a number was provided as an argument. If not, it generates a random number.
   number = chosenNumber || generateRandomNumber();
-
   /**
    * TODO: Declare and initialize the variables that are required to keep track of game state.
    */
   // Your code here.
-
+  let all_guess = [];
   // This is the core game loop where the user is prompted for guesses.
   const askForGuess = () => {
     getInput("Enter your guess: ", (guess) => {
-      // TODO: Implement the logic inside this function using recursive functions to achieve the game loop.
-      // Your code here.
+      all_guess.push(guess);
+      if (all_guess.length == 11) {
+        console.log("You are out of guesses.");
+        console.log("your guesses: ", all_guess);
+        exit(0);
+      }
+      if (number == guess) {
+        console.log("you won");
+        console.log(all_guess);
+        exit(0);
+      } else if (number > guess) {
+        console.log("too low");
+        askForGuess();
+      } else if (number < guess) {
+        console.log("too high");
+        askForGuess();
+      } else {
+        console.log("Wrong input!!!");
+        exit(1);
+      }
     });
   };
 
